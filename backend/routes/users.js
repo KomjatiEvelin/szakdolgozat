210bf02292af64b64dd97d4323f72776eea25f7e
KomjatiEvelin <komjatievelin@gmail.com>
2021-10-22 13:08:08 +0200

    making registration work

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var sha1 = require('sha1');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -24,11 +25,23 @@ router.get("/", function(req, res, next) {
 router.post('/register', function(req, res, next) {
   con.connect(function(err) {
     if (err) throw err;
-    con.query('insert into user(id,email,class,username) values(2,' + '\''+req.body.email + '\''+ ',' + req.body.class + ',' + '\''+ req.body.userName + '\''+')', function (error, results, fields) {
+    con.query('insert into user(email,class,username) values(' + '\''+req.body.email + '\''+ ',' + req.body.class + ',' + '\''+ req.body.userName + '\''+')', function (error, results, fields) {
+      if (error) throw error;
+    });
+    con.query('insert into passwords(user_id,passwd) values((select id from user where username=\''+req.body.userName+'\'),\''+sha1(req.body.passwd)+'\')', function (error, results, fields) {
       if (error) throw error;
       res.send(JSON.stringify(results));
     });
   });
 });
 
+router.post('/login', function(req, res, next) {
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query('', function (error, results, fields) { //TODO select-et megírni
+      if (error) throw error;
+      res.send(JSON.stringify(results));
+    });
+  });
+});
 module.exports = router;
--- frontend/src/components/views/RegPage.js
+++ frontend/src/components/views/RegPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Validation from 'react-validation';
 
 class RegPage extends React.Component {
 
@@ -11,8 +10,10 @@ class RegPage extends React.Component {
             passwd: "",
             class:""
         };
-        this.formOnChange = this.formOnChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
+        this.formOnChange=this.formOnChange.bind(this);
+
+
     }
 
     handleSubmit(event) {
@@ -23,7 +24,7 @@ class RegPage extends React.Component {
             passwd: this.state.passwd,
             class:this.state.class
         }
-        console.log(data)
+
         fetch("http://localhost:9000/users/register", {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
@@ -59,24 +60,24 @@ class RegPage extends React.Component {
                         <form method="POST" onSubmit={this.handleSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input onChange={this.formOnChange} type="email" className="form-control" id="exampleInputEmail1"
+                                <input onChange={this.formOnChange} type="email" className="form-control" name="email"
                                        aria-describedby="emailHelp"/>
                             </div>
                             <div className="mb-3">
-                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                <input onChange={this.formOnChange} type="username" className="form-control" id="exampleInputUsername1"/>
+                                <label htmlFor="exampleInputEmail1" className="form-label">User name</label>
+                                <input onChange={this.formOnChange} type="username" className="form-control" name="userName"/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                <input onChange={this.formOnChange} type="password" className="form-control" id="exampleInputPassword1"/>
+                                <input onChange={this.formOnChange} type="password" className="form-control" name="passwd"/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleInputPassword2" className="form-label">Password again</label>
-                                <input onChange={this.formOnChange} type="password" className="form-control" id="exampleInputPassword2"/>
+                                <input type="password" className="form-control" name="exampleInputPassword2"/>
                             </div>
                             <div className="mb-3">
                                 <label className="form-label" htmlFor="exampleClassk1">Class</label>
-                                <input onChange={this.formOnChange} type="number" className="form-control" id="exampleClass1"/>
+                                <input onChange={this.formOnChange} type="number" className="form-control" name="class"/>
                             </div>
                             <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
