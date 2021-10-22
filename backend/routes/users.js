1eac92c4005b5b189ffea374dfc94eaa7e528fd9
KomjatiEvelin <komjatievelin@gmail.com>
2021-10-22 13:12:21 +0200

    adding update method

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -44,4 +44,11 @@ router.post('/login', function(req, res, next) {
     });
   });
 });
+
+router.get('/edit', function(req, res, next) {
+  res.locals.connection.query('update members set class = \''+req.body.class+'\', email = \''+req.body.email+'\' where username = \''+req.body.userName+'\'', function (error, results, fields) {
+    if(error) throw error;
+    res.send(JSON.stringify(results));
+  });
+});
 module.exports = router;
